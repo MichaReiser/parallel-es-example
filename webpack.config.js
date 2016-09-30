9fad4b7c27256fbcc16150d282b22008a26d5d74
Micha Reiser <micha@famreiser.ch>
2016-09-30 13:32:21 +0200

    Working Example

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+var ParallelEsPlugin = require("parallel-es-webpack-plugin");
+
+const FILE_NAME = "[name].js";
+
+module.exports = {
+    devtool: "#source-map",
+    entry: {
+        examples: "./src/browser-example.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: FILE_NAME
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: "awesome-typescript-loader?useBabel"
+            }, {
+                test: /\.parallel\.js/,
+                loader: "source-map"
+            }
+        ]
+    },
+    plugins: [
+        new ParallelEsPlugin({})
+    ]
+};
