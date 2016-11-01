e6f381e1793def87fb01f634f2ff16f78ebd61b4
Micha Reiser <micha@famreiser.ch>
2016-11-01 22:12:36 +0100

    Disable uglify plugin for the moment

--- conf/webpack.prod.config.js
+++ conf/webpack.prod.config.js
@@ -5,9 +5,9 @@ module.exports = new Config().extend("conf/webpack.base.config.js").merge({
     devtool: "#source-map",
     plugins: [
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ // Seems to break worker source maps
-            sourceMap: true
-        })
+        new webpack.optimize.OccurrenceOrderPlugin()
+        // new webpack.optimize.UglifyJsPlugin({ // Seems to break worker source maps
+        //    sourceMap: true
+        //}) Besides breaks paralleljs tests because of renaming environment variables
     ]
 });
