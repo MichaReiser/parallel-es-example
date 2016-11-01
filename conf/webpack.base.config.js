5cf6d238531612b03c63ec2904f5d614d6fa3568
Micha Reiser <micha@famreiser.ch>
2016-11-01 17:16:41 +0100

    Create separate production build

---
+++ conf/webpack.base.config.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var webpack = require("webpack");
+var Config = require("webpack-config").Config;
+var ParallelEsPlugin = require("parallel-es-webpack-plugin");
+
+module.exports = new Config().merge({
+    entry: {
+        examples: "./src/browser-example.ts",
+        "performance-measurements": "./src/performance-measurement.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                exclude: path.resolve("./src/transpiled"),
+                loader: "babel!awesome-typescript-loader"
+            },
+            {
+                test: /\.ts$/,
+                include: path.resolve("./src/transpiled"),
+                loader: `babel?${JSON.stringify({"plugins": [ "parallel-es"] })}!awesome-typescript-loader`
+            },
+            {
+                test: /\.parallel-es6\.js/,
+                loader: "source-map"
+            }
+        ],
+        noParse: [
+            /benchmark\/benchmark\.js/,
+            /paralleljs\/lib\/parallel\.js/
+        ]
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+        modules:[
+              path.join(__dirname, "../node_modules")
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("common"),
+        new ParallelEsPlugin({
+            babelOptions: {
+                "presets": [
+                    ["es2015", { "modules": false }]
+                ]
+            }
+        })
+    ]
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,10 @@
-var path = require("path");
-var webpack = require("webpack");
-var ParallelEsPlugin = require("parallel-es-webpack-plugin");
+var process = require("process");
+var webpackConfig = require("webpack-config");
+var Config = webpackConfig.Config;
+var environment = webpackConfig.environment;
 
-module.exports = {
-    devtool: "#inline-source-map",
-    entry: {
-        examples: "./src/browser-example.ts",
-        "performance-measurements": "./src/performance-measurement.ts"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        pathinfo: true,
-        filename: "[name].js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                exclude: path.resolve("./src/transpiled"),
-                loader: "babel!awesome-typescript-loader"
-            },
-            {
-                test: /\.ts$/,
-                include: path.resolve("./src/transpiled"),
-                loader: `babel?${JSON.stringify({"plugins": [ "parallel-es"] })}!awesome-typescript-loader`
-            },
-            {
-                test: /\.parallel-es6\.js/,
-                loader: "source-map"
-            }
-        ],
-        noParse: [
-            /benchmark\/benchmark\.js/,
-            /paralleljs\/lib\/parallel\.js/
-        ]
-    },
-    resolve: {
-        extensions: [".webpack.js", ".web.js", ".ts", ".js"],
-        modules:[
-              path.join(__dirname, "node_modules")
-        ]
-    },
-    devServer: {
-        stats: {
-            chunks: false
-        }
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin("common"),
-        new ParallelEsPlugin({
-            babelOptions: {
-                "presets": [
-                    ["es2015", { "modules": false }]
-                ]
-            }
-        })
-    ]
-};
+environment.setAll({
+    env: function () { return process.env.NODE_ENV; }
+});
+
+module.exports = new Config().extend("conf/webpack.[env].config.js");
