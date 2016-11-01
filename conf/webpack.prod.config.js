185340fcab0169eb06e711860aac261bd46f6e3e
Micha Reiser <micha@famreiser.ch>
2016-11-01 22:20:45 +0100

    Reenablel Minification

--- conf/webpack.prod.config.js
+++ conf/webpack.prod.config.js
@@ -5,9 +5,12 @@ module.exports = new Config().extend("conf/webpack.base.config.js").merge({
     devtool: "#source-map",
     plugins: [
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin()
-        // new webpack.optimize.UglifyJsPlugin({ // Seems to break worker source maps
-        //    sourceMap: true
-        //}) Besides breaks paralleljs tests because of renaming environment variables
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ // Seems to break worker source maps
+            sourceMap: true,
+            mangle: {
+                except: ["global"] // do not mangle global paralleljs env
+            }
+        })
     ]
 });
