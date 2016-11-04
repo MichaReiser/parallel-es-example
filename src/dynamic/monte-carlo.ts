bf135879676b985199b0e002d7b9df88d58d0fe9
Micha Reiser <micha@famreiser.ch>
2016-11-04 21:00:29 +0100

    Upgrade Parallel-es to 0.1.8

--- src/dynamic/monte-carlo.ts
+++ src/dynamic/monte-carlo.ts
@@ -332,7 +332,7 @@ export function syncMonteCarlo(options?: IMonteCarloSimulationOptions) {
 export function parallelMonteCarlo(userOptions?: IMonteCarloSimulationOptions) {
     const options = initializeOptions(userOptions);
     return parallel
-        .from(options.projects, { minValuesPerTask: 2, oversubscribe: false })
+        .from(options.projects, { minValuesPerTask: 2, maxDegreeOfParallelism: parallel.defaultOptions().maxConcurrencyLevel })
         .inEnvironment(createMonteCarloEnvironment, options)
         .map(calculateProject);
 }
