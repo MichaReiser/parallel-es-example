10398d28188058d8d16fbb28d140875fa9bdcf3c
Travis CI <micha@famreiser.ch>
2016-11-21 06:53:43 +0000

    Deploy to GitHub Pages: 114efad5bb1e31472cdbf88f2892b0202d89be91

---
+++ benchmarks/charts.R
@@ -0,0 +1,115 @@
+#!/usr/bin/env Rscript
+require( tikzDevice )
+
+percent <- function(x, digits = 2, format = "f", ...) {
+  ifelse(is.na(x), "", formatC(100 * x, format = format, digits = digits, ...))
+}
+
+time <- function (x, ...) {
+  ifelse(x < 100, 
+         ifelse(x < 10, paste0(formatC(x, format = "f", digits = 2, ...), "s"), paste0(formatC(x, format = "f", digits = 1, ...), "s")),
+         paste0(formatC(x, format = "f", digits = 0, ...), "s"))
+}
+
+dir.create(file.path("charts"), showWarnings=FALSE)
+data <- read.csv("./benchmarks.csv")
+
+computeRelativeMean <- function (record) {
+  syncBaseLine <- data[data$OS == record["OS"] & data$OS.Version == record["OS.Version"] & data$Browser == record["Browser"] & data$Browser.Version==record["Browser.Version"] & data$Set == "sync" & data$Name == record["Name"],]
+  as.numeric(record["Mean..s."]) / syncBaseLine[1, "Mean..s."]
+}
+
+plotChart <- function (plotData, byBrowserVersion, environmentTitle=NULL) {
+  colours <- gray.colors(nrow(plotData), start=0.4, end=1)
+  barDensity <- c(-10, 40, 25, 0)[1:nrow(plotData)]
+  shadeAngle <- c(0, 45, -45, 0)[1:nrow(plotData)]
+  fontColors <- c("white", "black", "black", "black")[1:nrow(plotData)]
+  
+  # Reduce margin, mainly for latex output
+  #par(mar=c(2,4,2,0))
+  bb <- barplot(plotData, beside=TRUE, main=environmentTitle, ylab="Relative to Sync (%)", col=colours, ylim = c(0, max(1, max(plotData+0.2, na.rm=TRUE))), density = barDensity, angle=shadeAngle)
+  legend("topleft", legend = rownames(plotData), bty="n", fill=colours, density = barDensity, angle=shadeAngle)
+  text(bb, plotData, percent(plotData, digits = 1), pos = 3, cex = 0.8, col="black")
+  
+  means <- with(byBrowserVersion, {
+    out <- matrix(nrow=nlevels(byBrowserVersion$Set), ncol=nlevels(byBrowserVersion$Name),
+                  dimnames=list(levels(byBrowserVersion$Set), levels(byBrowserVersion$Name)))
+    out[cbind(byBrowserVersion$Set, byBrowserVersion$Name)] <- byBrowserVersion$Mean..s.
+    out <- out[rowSums(is.na(out)) != ncol(out),]
+    out <- out[, colSums(is.na(out)) != nrow(out)]
+    out
+  })
+  
+  text(bb, 0, time(means), pos=3, cex=0.8, col=fontColors)
+}
+
+
+data$RelativeMean <- apply(data, 1, computeRelativeMean)
+data <- data[!is.na(data$RelativeMean), ]
+
+createCharts <- function (sets, tests) {
+  relevant <- data[data$Set %in% sets & data$Name %in% tests, ]
+
+  for(os in unique(relevant$OS)) {
+    byOS <- relevant[relevant$OS==os,]
+    
+    for (osVersion in unique(byOS$OS.Version)) {
+      byOSVersion <- byOS[byOS$OS.Version == osVersion, ]
+      
+      for (browser in unique(byOSVersion$Browser)) {
+        byBrowser <- byOSVersion[byOSVersion$Browser==browser, ]
+        
+        for (browserVersion in unique(byBrowser$Browser.Version)) {
+          byBrowserVersion <- byBrowser[byBrowser$Browser.Version==browserVersion, ]
+          osName <- paste0(os, " ", osVersion);
+          dir.create(file.path("charts", osName), showWarnings=FALSE)
+          
+          environmentTitle = paste(os, osVersion, browser, browserVersion)
+          
+          pictureName <- paste0(browser, "-", browserVersion)
+          fullName <- paste0("charts/", osName, "/", pictureName)
+        
+          tmp <- byBrowserVersion[, c("Set", "Name", "RelativeMean")]
+          plotData <- with(tmp, {
+            out <- matrix(nrow=nlevels(tmp$Set), ncol=nlevels(tmp$Name),
+                          dimnames=list(levels(tmp$Set), levels(tmp$Name)))
+            out[cbind(tmp$Set, tmp$Name)] <- tmp$RelativeMean
+            out <- out[rowSums(is.na(out)) != ncol(out),]
+            out <- out[, colSums(is.na(out)) != nrow(out)]
+            out
+          })
+          
+          png(paste0(fullName, ".png"), height=16, width=ncol(plotData) * 9, units="cm", res=300)
+          plotChart(plotData, byBrowserVersion, environmentTitle)
+          dev.off()
+          
+          svg(paste0(fullName, ".svg"), height=6.29921, width=ncol(plotData) * 3.54331)
+          plotChart(plotData, byBrowserVersion, environmentTitle)
+          dev.off()
+          
+          tikz(paste0(fullName, ".tex"), height=2.9, sanitize = TRUE, engine="pdftex")
+          plotChart(plotData, byBrowserVersion)
+          dev.off()
+        }
+      }
+    }
+  }  
+}
+
+createCharts(
+  c("Parallel.es", "Hamsters.js", "Threads.js", "Parallel.js"),
+  c("Knights Tour (5x5)", "Knights Tour (6x6)", "Mandelbrot", "Riskprofiling")
+)
+
+# createCharts(
+#  c("parallel-dynamic", "hamstersjs"), 
+#  c("Monte Carlo Math.random 100k", "Monte Carlo Math.random 1m"),
+#  "Monte Carlo simjs"
+#)
+
+#createCharts(
+#  c("parallel-es", "paralleljs", "threadsjs"), 
+#  c("Monte Carlo simjs 100k", "Monte Carlo simjs 1m"),
+#  "Monte Carlo Math.random"
+#)
+
---
+++ benchmarks/convert-to-csv.js
@@ -0,0 +1,113 @@
+const json2csv  = require("json2csv");
+const fs = require("fs");
+const path = require("path");
+
+const directories = ["sync", "parallel-dynamic", "parallel-transpiled", "paralleljs", "threadsjs", "hamstersjs" ];
+
+const outputs = [];
+const fields = [
+    {
+        label: "Set",
+        value: "directory",
+    }, {
+        label: "File",
+        value: "file"
+    }, {
+        label: "Name",
+        value: function (row) {
+            const [set, ...nameParts] = row.benchmarks.name.split(":");
+            let cleanName = nameParts.join(":").trim().replace(/\.0/g, ",0"); // Unify Monte carlo names
+
+            if (cleanName.endsWith("(projects: 8, runs: 1,000,000)")) {
+                cleanName = cleanName.replace("(projects: 8, runs: 1,000,000)", "");
+            }
+
+            if (cleanName.endsWith("(projects: 8, runs: 100,000)")) {
+                cleanName = cleanName.replace("(projects: 8, runs: 100,000)", "100k");
+            }
+
+            cleanName = cleanName.replace("simjs", "");
+            cleanName = cleanName.replace("Monte Carlo", "Riskprofiling");
+
+            if (cleanName === "Mandelbrot 10000x10000, 1000") {
+                cleanName = "Mandelbrot";
+            }
+
+            return cleanName.trim();
+        }
+    },{
+        label: "Margin of Error",
+        value: "benchmarks.stats.moe"
+    }, {
+        label: "Relative Margin of Error",
+        value: "benchmarks.stats.rme"
+    }, {
+        label: "Standard Error of the mean",
+        value: "benchmarks.stats.sem"
+    }, {
+        label: "Standard Deviation",
+        value: "benchmarks.stats.deviation"
+    }, {
+        label: "Mean (s)",
+        value: "benchmarks.stats.mean"
+    }, {
+        label: "Variance",
+        value: "benchmarks.stats.variance"
+    }, {
+        label: "Time Stamp",
+        value: function (row) {
+            // http://stackoverflow.com/questions/1703505/excel-date-to-unix-timestamp
+            const timestamp = row.benchmarks.times.timeStamp;
+            return new Date(timestamp).toISOString().split(".")[0].replace("T", " ");
+        }
+    }, {
+        label: "Browser",
+        value: "platform.name"
+    }, {
+        label: "Browser Version",
+        value: "platform.version"
+    }, {
+        label: "OS",
+        value: "platform.os.family"
+    }, {
+        label: "OS Version",
+        value: "platform.os.version"
+    }, {
+        label: "OS Architecture",
+        value: "platform.os.architecture"
+    }
+];
+
+for (const directory of directories) {
+    for (const file of fs.readdirSync(path.resolve(directory))) {
+        if (!file.endsWith(".json")) {
+            continue;
+        }
+
+        const first = outputs.length === 0;
+
+        const fileContent = fs.readFileSync(path.join(__dirname, directory, file), "utf-8");
+        const json = JSON.parse(fileContent);
+
+
+        let set = directory;
+        if (set === "paralleljs") {
+            set = "Parallel.js";
+        } else if (set === "hamstersjs") {
+            set = "Hamsters.js";
+        } else if (set === "threadsjs") {
+            set = "Threads.js";
+        }
+        if (set === "parallel-transpiled") {
+            set = "Parallel.es";
+        } else if (set === "parallel-dynamic") {
+            set = "parallel-es-dynamic";
+        }
+
+        json.directory = set;
+        json.file =  file;
+        outputs.push(json2csv({ data: json, fields, hasCSVColumnTitle: first, unwindPath: "benchmarks"}));
+    }
+}
+
+fs.writeFileSync(path.join(__dirname, "benchmarks.csv"), outputs.join("\n"));
