c9eb789a8f9e69c9289b404e3fbb40d74f1ba29b
Micha Reiser <micha@famreiser.ch>
2016-11-27 18:30:29 +0100

    Upgrade Dependencies

--- src/transpiled/knights-tour.ts
+++ src/transpiled/knights-tour.ts
@@ -108,7 +108,7 @@ export function parallelKnightTours(start: ICoordinate, boardSize: number): Prom
     }
 
     return parallel
-        .from(computeStartFields(), {maxValuesPerTask: 1}) // compute each path separately to achieve good cpu usage)
+        .from(computeStartFields())
         .inEnvironment(createEnvironment, boardSize)
         .map(knightTours)
         .reduce(0, (memo, count) => memo + count);
